eb90532d5b69da95674da4c2eccf1e21e7199cf3
somethingrsy-rgb <somethingrsy@gmail.com>
2026-09-26 00:16:14 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -34,7 +34,7 @@ self.addEventListener("notificationclick", (event) => {
   event.waitUntil(clients.openWindow(event.notification?.data?.url || "./index.html"));
 });
 
-const CACHE_NAME = "director-queue-pwa-v6-urgent-dedup";
+const CACHE_NAME = "director-queue-pwa-v7-realtime";
 const CORE_ASSETS = [
   "./",
   "./index.html",
